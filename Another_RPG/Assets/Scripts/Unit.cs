0631492ced2f807c74d4370ce10eaaae531b9497
TheG1uy <paulnvidia@gmail.com>
2018-03-06 21:18:39 +0300

    Create start_class_unit and stick

---
+++ Another_RPG/Assets/Scripts/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+	protected int health_point=1;
+	[SerializeField]
+	protected float speed=1.0f;
+	protected virtual void Move(){
+	}
+	protected virtual void DamageRecive(){
+		gameObject.SetActive (false);
+	}
+
+}
