b651ba1b75e0ddb288ecefa7ca0483e6c7a3aaf9
owerbat <niktimak@gmail.com>
2018-03-31 13:02:08 +0300

    The bullet was created

---
+++ Another_RPG/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10.0f;
+    private Vector3 direction;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+}
