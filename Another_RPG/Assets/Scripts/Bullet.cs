355a7cb0504df80933072c73c815dd140a2675af
owerbat <niktimak@gmail.com>
2018-03-31 14:27:13 +0300

    First version of range mob was added

--- Another_RPG/Assets/Scripts/Bullet.cs
+++ Another_RPG/Assets/Scripts/Bullet.cs
@@ -7,9 +7,15 @@ public class Bullet : MonoBehaviour
     [SerializeField]
     private float speed = 10.0f;
     private Vector3 direction;
+    public Vector3 Direction { set { direction = value; } }
 
     private void FixedUpdate()
     {
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, 1.5f);
     }
 }
---
+++ Another_RPG/Assets/Scripts/Enemies/RangeMob.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RangeMob : Enemy {
+    private Bullet bullet;
+    private DateTime lastShotTime;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        bullet = Resources.Load<Bullet>("Bullet");
+        lastShotTime = DateTime.Now;
+    }
+
+    private void FixedUpdate()
+    {
+        direction = Mathf.Sign(target.transform.position.x - transform.position.x);
+
+        if (Math.Abs(DateTime.Now.Second - lastShotTime.Second) >= 3)
+        {
+            Shoot();
+            lastShotTime = DateTime.Now;
+        }
+    }
+
+    private void Shoot()
+    {
+        Vector3 position = transform.position;
+
+        Bullet newBullet = Instantiate(bullet, position, bullet.transform.rotation) as Bullet;
+        newBullet.Direction = new Vector3(direction, 0);
+    }
+}
